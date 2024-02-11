038357717c5f4fdd3407aed720fb3031ddf0847b
akhilbz <akhilesh.bitla@gmail.com>
2024-02-11 04:40:02 -0600

    Created new page for Refugee Testimonials

---
+++ group6/src/pages/refugee-testimonials.js
@@ -0,0 +1,15 @@
+import NavBar from './NavBar'; // Adjust the path as necessary
+
+export default function RefugeeTestimonials() {
+    return (
+        <div>
+            <NavBar />
+            <main>
+                <h1>Refugee Testimonials</h1>
+                <p>This is the Refugee Testimonials page.</p>
+                // Your content goes here
+            </main>
+        </div>
+    );
+};
+
