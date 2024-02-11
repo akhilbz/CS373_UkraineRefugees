db9d9f0f66d7b2346fad88537a45761413a410c5
AlexJimenez12 <ajimenez1173@gmail.com>
2024-02-10 20:45:46 -0600

    Finished Splash Page.

--- group6/src/pages/index.js
+++ group6/src/pages/index.js
@@ -1,4 +1,5 @@
 import NavBar from './NavBar'; // Import the NavBar component
+import Image from 'next/image'; // Import the Image component
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -8,8 +9,14 @@ export default function Home() {
       {/* Home Screen */}
       <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${styles.mainContent}`}>
         <div className={styles.mainContent}>
-          <h1>Ukraine Refugee</h1>
-          <p>
+          <h1 className={styles.title}>Ukraine Refugee</h1>
+          <div className={styles.imageContainer}>
+            {/* Ukraine picture */}
+            <div className={styles.imageWrapper}>
+              <Image src="/Ukraine-Picture.jpeg" alt="Ukraine Picture" width={900} height={600} />
+            </div>
+          </div>
+          <p className={`${styles.paragraph} ${styles.italic}`}>
             The ongoing situation in Ukraine has led to a humanitarian crisis, with many individuals displaced and in need of assistance. Our group is dedicated to promoting resources and aid to those affected by the crisis. Together, we strive to provide support and raise awareness for the Ukraine refugee community.
           </p>
           <p className={styles.italic}>"United in solidarity, we can make a difference."</p>
