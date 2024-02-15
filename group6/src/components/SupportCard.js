3a3b9e70fd0f1f642fdf873a67742d1052ab469e
AlexJimenez12 <ajimenez1173@gmail.com>
2024-02-14 20:54:15 -0600

    Adding attributes for support groups.

--- group6/src/components/SupportCard.js
+++ group6/src/components/SupportCard.js
@@ -4,27 +4,35 @@ import Link from 'next/link';
 export default function SupportCard({ support_groups_data }) {
 
     return (
-        <div className="flex flex-col rounded-2xl h-full"> {/* Ensure full height and column layout */}
+        <div className="flex flex-col rounded-2xl h-full overflow-hidden"> {}
             <div className="flex-grow"> {/* Flex-grow to fill space */}
-                <div className="rounded-t-2xl  flex min-h-[50px] max-h-[170px] justify-center items-center bg-yellow-600">
+                <div className="rounded-t-2xl flex items-center justify-center bg-yellow-600 h-[50px]">
                     <h1 className="text-white text-2xl font-light text-center">{support_groups_data.organization}</h1>
                 </div>
                 <div className="border-b-[1px]">
                     <div className="m-2">
-                        <h1 className="font-semibold">{support_groups_data.organization}</h1>
+                        <h1 className="font-semibold text-sm">{support_groups_data.organization}</h1>
                         <div className="flex justify-between">
-                            <p className="text-sm">Status: <span className="text-green-600">{support_groups_data.status}</span></p>
-                            <p className="text-sm">Year: {support_groups_data.year}</p>
+                            <p className="text-xs">Status: <span className="text-green-600">{support_groups_data.status}</span></p>
+                            <p className="text-xs">Year: {support_groups_data.year}</p>
                         </div>
                     </div>
                 </div>
                 <div className="m-2 overflow-ellipsis"> {/* Content area */}
-                    <p className=" text-sm">{support_groups_data.caption}</p>
+                    <p className="text-xs"><span className="font-semibold">Services provided: </span>{support_groups_data.caption}</p>
+                </div>
+                {/* New line for "Based In" */}
+                <div className="m-2">
+                    <p className="text-xs"><span className="font-semibold">Based In: </span>{support_groups_data.location}</p>
+                </div>
+                {/* New line for "Contact" */}
+                <div className="m-2">
+                    <p className="text-xs"><span className="font-semibold">Contact: </span>{support_groups_data.contact}</p>
                 </div>
             </div>
-            <div className="flex justify-center pb-1"> {/* Button at the bottom */}
+            <div className="flex justify-center pb-1"> {}
                 <Link href={`/support-groups/${support_groups_data.id}`} key={support_groups_data.id} passHref>
-                    <button className="h-[25px] w-[85px] bg-slate-300 rounded-lg">Explore</button>
+                    <button className="h-[25px] w-[85px] bg-slate-300 rounded-lg text-xs">Explore</button>
                 </Link>
             </div>
         </div>
--- group6/src/pages/index.js
+++ group6/src/pages/index.js
@@ -174,81 +174,95 @@ export default function Home() {
       link: 'https://airlinkflight.org/responses/ukraine/',
       resources: ['https://airlinkflight.org/help-ukraine/', 'https://airlinkflight.org/get-involved/ways-to-give/miles/'],
       youtube: 'https://www.youtube.com/embed/Asr-EWQ_IGQ?si=CH6qbyakPU1YhOQz',
-      picture: 'https://www.adra.ua/wp-content/uploads/2023/03/airlink.jpg'
-  },
-  {
-      id: 2,
-      organization: "CARE (Cooperative For Assistance And Relief Everywhere)",
-      status: "active",
-      year: "2022",
-      caption: `CARE works around the globe to save lives, defeat poverty and achieve social justice.`,
-      about: `CARE works around the globe to save lives, defeat poverty and achieve social justice. 
+      picture: 'https://www.adra.ua/wp-content/uploads/2023/03/airlink.jpg',
+      location: 'Washington DC',
+      contact: '+1 (202) 375-3434'
+
+    },
+      {
+        id: 2,
+        organization: "CARE",
+        status: "active",
+        year: "2022",
+        caption: `CARE works around the globe to save lives, defeat poverty and achieve social justice.`,
+        about: `CARE works around the globe to save lives, defeat poverty and achieve social justice. 
       CARE has launched a humanitarian appeal to support the most vulnerable Ukrainians, particularly women-headed households and the elderly. 
       Emergency gifts support CARE’s Ukraine Crisis Fund to reach 4 million with immediate aid and recovery, food, water, hygiene kits, 
       psychosocial support, and cash assistance — prioritizing women and girls, families, and the elderly.`,
-      link: 'https://www.care.org/our-work/where-we-work/ukraine/',
-      resources: ['https://my.care.org/site/Donation2?df_id=31067&mfc_pref=T&31067.donation=form1&s_src=172220UCF000&_ga=2.30464188.2031879431.1646163164-1963073507.1646163164', 'https://www.airbnb.org/help-ukraine'],
-      youtube: 'https://www.youtube.com/embed/OICJBJgWT20?si=NVA3bX7ld0GAJFrX',
-      picture: 'https://www.care.org/wp-content/uploads/2021/01/primary_logo_horizontal_min.svg'
-  },
-  {
-      id: 3,
-      organization: "Direct Relief",
-      status: "active",
-      year: "2024",
-      caption: `A Santa Barbara-based organization that works to equip health professionals in resource-poor communities to meet the challenges of caring for people in need.`,
-      about: `A Santa Barbara-based organization that works to equip health professionals in resource-poor communities to meet the challenges of caring for people in need. 
+        link: 'https://www.care.org/our-work/where-we-work/ukraine/',
+        resources: ['https://my.care.org/site/Donation2?df_id=31067&mfc_pref=T&31067.donation=form1&s_src=172220UCF000&_ga=2.30464188.2031879431.1646163164-1963073507.1646163164', 'https://www.airbnb.org/help-ukraine'],
+        youtube: 'https://www.youtube.com/embed/OICJBJgWT20?si=NVA3bX7ld0GAJFrX',
+        picture: 'https://www.care.org/wp-content/uploads/2021/01/primary_logo_horizontal_min.svg',
+        location: 'Atlanta, GA',
+        contact: '1-800-422-7385',
+      },
+      {
+        id: 3,
+        organization: "Direct Relief",
+        status: "active",
+        year: "2024",
+        caption: `A Santa Barbara-based organization that works to equip health professionals in resource-poor communities to meet the challenges of caring for people in need.`,
+        about: `A Santa Barbara-based organization that works to equip health professionals in resource-poor communities to meet the challenges of caring for people in need. 
       Direct Relief received a list from the Ukrainian Ministry of Health of needed items, such as medications and mobile medical outreach packs with CAT-style tourniquets and bandages. 
       Direct Relief has also received information indicating a dire need for tranexamic acid, which is used to control severe bleeding by blocking the breakdown of blood clots. 
       They recently sent a large shipment of diabetic supplies and they are offering Ukrainian partners IV fluids, antibiotics, medications for anesthesia, sutures, and cardiovascular medication.`,
-      link: 'https://www.directrelief.org/',
-      resources: ['https://secure.directrelief.org/site/Donation2?df_id=2924&mfc_pref=T&2924.donation=form1', 'https://www.directrelief.org/get-involved/donate-crypto/',
-      'https://www.weho.org/services/human-services/resources-for-ukrainian-refugees#:~:text=visit%20their%20website-,here,-.'],
-      youtube: 'https://www.youtube.com/embed/MzPBYv1hG34?si=TDOvSEOpdD-BXhf9',
-      picture: 'https://mma.prnewswire.com/media/1018033/Direct_Relief_Logo.jpg?p=facebook'
-  },
-  {
-      id: 4,
-      organization: "Doctors without Borders",
-      status: "active",
-      year: "2024",
-      caption: `Doctors Without Borders/Médecins Sans Frontières is an independent, global nonprofit organization providing medical aid where it’s needed most.`,
-      about: `Doctors Without Borders/Médecins Sans Frontières is an independent, global nonprofit organization providing medical aid where it’s needed most. 
+        link: 'https://www.directrelief.org/',
+        resources: ['https://secure.directrelief.org/site/Donation2?df_id=2924&mfc_pref=T&2924.donation=form1', 'https://www.directrelief.org/get-involved/donate-crypto/',
+          'https://www.weho.org/services/human-services/resources-for-ukrainian-refugees#:~:text=visit%20their%20website-,here,-.'],
+        youtube: 'https://www.youtube.com/embed/MzPBYv1hG34?si=TDOvSEOpdD-BXhf9',
+        picture: 'https://mma.prnewswire.com/media/1018033/Direct_Relief_Logo.jpg?p=facebook',
+        location: 'Global',
+        contact: '1-805-964-4767'
+      },
+      {
+        id: 4,
+        organization: "Doctors without Borders",
+        status: "active",
+        year: "2024",
+        caption: `Doctors Without Borders/Médecins Sans Frontières is an independent, global nonprofit organization providing medical aid where it’s needed most.`,
+        about: `Doctors Without Borders/Médecins Sans Frontières is an independent, global nonprofit organization providing medical aid where it’s needed most. 
       Emergency teams have arrived at the Polish-Ukrainian border and are currently trying to get essential staff and supplies into Ukraine. 
       They are working to set up emergency response activities and dispatching teams to Poland, Moldova, Hungary, Romania, and Slovakia.`,
-      link: 'https://www.doctorswithoutborders.org/',
-      resources: ['https://donate.doctorswithoutborders.org/secure/donate?_ga=2.127093709.1983195640.1646178330-1517370144.1646178330'],
-      youtube: 'https://www.youtube.com/embed/f0dY7qazyQE?si=LTwd-cVBFMikmxF1',
-      picture: 'https://www.doctorswithoutborders.org/themes/custom/msf/meta_image.png'
-  },
-  {
-      id: 5,
-      organization: "Global Empowerment Mission",
-      status: "active",
-      year: "2022",
-      caption: `Global Empowerment Mission (GEM) is a nonprofit organization founded as a first responder to global disasters.`,
-      about: `Global Empowerment Mission (GEM) is a nonprofit organization founded as a first responder to global disasters. 
+        link: 'https://www.doctorswithoutborders.org/',
+        resources: ['https://donate.doctorswithoutborders.org/secure/donate?_ga=2.127093709.1983195640.1646178330-1517370144.1646178330'],
+        youtube: 'https://www.youtube.com/embed/f0dY7qazyQE?si=LTwd-cVBFMikmxF1',
+        picture: 'https://www.doctorswithoutborders.org/themes/custom/msf/meta_image.png',
+        location: "Geneva, Switzerland",
+        contact: '212.679.6800'
+      },
+      {
+        id: 5,
+        organization: "Global Empowerment Mission",
+        status: "active",
+        year: "2022",
+        caption: `Global Empowerment Mission (GEM) is a nonprofit organization founded as a first responder to global disasters.`,
+        about: `Global Empowerment Mission (GEM) is a nonprofit organization founded as a first responder to global disasters. 
       GEM has established a welcome center in Medyka, Poland on the Ukraine border. The Welcome Center will serve as a relocation and aid center. 
       Ukrainian evacuees can choose a country where they have family or friends that can take them in. 
       Donations will assist in relocating thousands of women and children throughout Europe and funds will be used for booking and paying for immediate transportation.`,
-      link: 'https://www.globalempowermentmission.org/mission/ukraine-crisis/',
-      resources: ['https://globalempowermentmission.kindful.com/?campaign=1182522'],
-      youtube: 'https://www.youtube.com/embed/xFxDpPJZbSo?si=APg-Iwxe6-931IJP',
-      picture: 'https://www.globalempowermentmission.org/wp-content/uploads/2020/08/gem-logo-social.jpg'
-  },
-  {
-      id: 6,
-      organization: "GlobalGiving",
-      status: "active",
-      year: "2022",
-      caption: `GlobalGiving is a nonprofit that supports other nonprofits by connecting them to donors.`,
-      about: `GlobalGiving is a nonprofit that supports other nonprofits by connecting them to donors. 
+        link: 'https://www.globalempowermentmission.org/mission/ukraine-crisis/',
+        resources: ['https://globalempowermentmission.kindful.com/?campaign=1182522'],
+        youtube: 'https://www.youtube.com/embed/xFxDpPJZbSo?si=APg-Iwxe6-931IJP',
+        picture: 'https://www.globalempowermentmission.org/wp-content/uploads/2020/08/gem-logo-social.jpg',
+        location: 'Doral, Florida',
+        contact: '(800) 995-7604'
+      },
+      {
+        id: 6,
+        organization: "GlobalGiving",
+        status: "active",
+        year: "2022",
+        caption: `GlobalGiving is a nonprofit that supports other nonprofits by connecting them to donors.`,
+        about: `GlobalGiving is a nonprofit that supports other nonprofits by connecting them to donors. 
       All donations to the Ukraine Crisis Relief Fund will support humanitarian assistance in impacted communities in Ukraine and surrounding regions where Ukrainian refugees have fled. 
       GlobalGiving's nonprofit partners are providing shelter, food, clean water, psychosocial support, and economic assistance to displaced communities.`,
-      link: 'https://www.globalgiving.org/projects/ukraine-crisis-relief-fund/',
-      resources: ['https://www.globalgiving.org/projects/ukraine-crisis-relief-fund/'],
-      youtube: 'https://www.youtube.com/embed/Sic0rsdnglY?si=p7sD5eFMaDCmRJV3',
-      picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-hX98ipa3t9IA3VzudSwcyYMXsIzFGSXnff2_TLFDd2u1zCdDry0jEBOHCxwhcSTewUU&usqp=CAU'
+        link: 'https://www.globalgiving.org/projects/ukraine-crisis-relief-fund/',
+        resources: ['https://www.globalgiving.org/projects/ukraine-crisis-relief-fund/'],
+        youtube: 'https://www.youtube.com/embed/Sic0rsdnglY?si=p7sD5eFMaDCmRJV3',
+        picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-hX98ipa3t9IA3VzudSwcyYMXsIzFGSXnff2_TLFDd2u1zCdDry0jEBOHCxwhcSTewUU&usqp=CAU',
+        location: 'Washington, D.C., United States',
+        contact: '+1-202-232-5784'
+
   }];
 
   useEffect(() => {
