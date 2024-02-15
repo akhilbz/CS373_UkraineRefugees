4d714107acbe7bc22d25d9cb8616c8cec17337ae
Derek Chen <derex1987@gmail.com>
2024-02-14 20:06:00 -0600

    Connections!

--- group6/src/pages/news-and-media/[id].js
+++ group6/src/pages/news-and-media/[id].js
@@ -2,7 +2,7 @@ import NavBar from '../NavBar';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 import { useJsApiLoader, GoogleMap, Marker, } from '@react-google-maps/api';
-import Grid from '@mui/material/Grid'; 
+import Grid from '@mui/material/Grid';
 
 export default function TestimonialPage() {
   const router = useRouter();
@@ -15,7 +15,7 @@ export default function TestimonialPage() {
   })
 
   // Use useSelector without await, as it is synchronous
-  const newsMedia = useSelector(state => 
+  const newsMedia = useSelector(state =>
     state.newsMedia.find((item, index) => index === parseInt(id, 10) - 1)
   );
 
@@ -33,19 +33,19 @@ export default function TestimonialPage() {
     <div>
       <NavBar />
       <main className='flex flex-col items-center pt-7'>
-        <h1 className=' pt-5 pb-5 flex justify-center text-3xl font-sans'>{newsMedia?.title ?? "_topic_" }</h1>
+        <h1 className=' pt-5 pb-5 flex justify-center text-3xl font-sans'>{newsMedia?.title ?? "_topic_"}</h1>
         <div className="flex pb-5 justify-center">
           <div className='rounded-2xl w-[1000px] h-auto pb-8 bg-white bg-opacity-40'>
             <div className="flex justify-between">
               <div className="flex justify-start p-3 w-full">
-                <img src={newsMedia?.image  ?? "_img_url_"} className=" max-h-[250px] rounded-2xl "/>
+                <img src={newsMedia?.image ?? "_img_url_"} className=" max-h-[250px] rounded-2xl " />
               </div>
               <div className='w-full'>
                 <div className=" rounded-tr-2xl pb-2 flex items-end flex-col justify-end w-auto h-auto bg-yellow-600">
-                    <h1 className=' py-1 pr-3 flex justify-end text-xl font-light'>{`Publisher: ${newsMedia?.publisher ?? "_publisher_"}`}</h1>
-                    <h2 className=' flex w-full justify-end pr-3 text-xl font-light'>{`Date Published: ${newsMedia?.date ?? "_date_"}`}</h2>
-                    <h2 className=' flex w-full justify-end pr-3 text-xl font-light'>{`Source Type: ${newsMedia?.source ?? "_source_"}`}</h2>
-                    <h2 className=' flex w-full justify-end pr-3 text-xl font-light'>{`Location: ${newsMedia?.location ?? "_location_"}`}</h2>
+                  <h1 className=' py-1 pr-3 flex justify-end text-xl font-light'>{`Publisher: ${newsMedia?.publisher ?? "_publisher_"}`}</h1>
+                  <h2 className=' flex w-full justify-end pr-3 text-xl font-light'>{`Date Published: ${newsMedia?.date ?? "_date_"}`}</h2>
+                  <h2 className=' flex w-full justify-end pr-3 text-xl font-light'>{`Source Type: ${newsMedia?.source ?? "_source_"}`}</h2>
+                  <h2 className=' flex w-full justify-end pr-3 text-xl font-light'>{`Location: ${newsMedia?.location ?? "_location_"}`}</h2>
                 </div>
                 <div className="bg-white rounded-bl-2xl">
                   <p className='p-2  text-lg text-yellow-600 flex justify-end w-full font-light'>{`"${newsMedia?.caption ?? "_caption_"}"`}</p>
@@ -53,28 +53,46 @@ export default function TestimonialPage() {
               </div>
             </div>
             <div className='pt-2 pb-4 w-full flex justify-center h-[350px] rounded-[50px]'>
-            <GoogleMap 
-              center={center}
-              zoom={5}
-              mapContainerStyle={{ width: '500px', height: '350px', borderRadius: '50px'  }}
-              options={{
-                zoomControl: false,
-                streetViewControl: false,
-                mapTypeControl: false,
-                fullscreenControl: false,
-              }}>
-              <Marker position={center} />
-            </GoogleMap>
+              <GoogleMap
+                center={center}
+                zoom={5}
+                mapContainerStyle={{ width: '500px', height: '350px', borderRadius: '50px' }}
+                options={{
+                  zoomControl: false,
+                  streetViewControl: false,
+                  mapTypeControl: false,
+                  fullscreenControl: false,
+                }}>
+                <Marker position={center} />
+              </GoogleMap>
             </div>
             <div className="w-[1000px] flex flex-col  items-center justify-between">
-                <div className="w-[750px] flex justify-start pb-2">
-                    <h1 className='text-2xl font-light'>Read More at:</h1>
-                </div>
-                <div className='rounded-2xl flex justify-center items-center w-[800px] h-[50px]  bg-white'>
+              <div className="w-[750px] flex justify-start pb-2">
+                <h1 className='text-2xl font-light'>Read More at:</h1>
+              </div>
+              <div className='rounded-2xl flex justify-center items-center w-[800px] h-[50px]  bg-white'>
                 <h1 className='text-center text-md text-yellow-600'><span className=' hover:underline'>
-                    <a href={newsMedia?.link ?? 'www.google.com'} target="_blank" rel="noopener noreferrer">{newsMedia?.link ?? 'www.google.com'}</a></span></h1>
-                </div>
+                  <a href={newsMedia?.link ?? 'www.google.com'} target="_blank" rel="noopener noreferrer">{newsMedia?.link ?? 'www.google.com'}</a></span></h1>
+              </div>
             </div>
+          </div>
+          {/* Connections */}
+          <div className="w-full pt-5 pb-5">
+            <div className="w-full flex justify-center text-3xl pb-5 font-light">
+              <h1>Recent News and Resources:</h1>
+            </div>
+            <Grid container spacing={3} className='flex justify-center '>
+              <Grid item xs={6} md={2.5} className='flex justify-center '>
+                <Card className='rounded-2xl h-[400px] w-[275px]'>
+                  <SupportCard support_groups_data={supportGroups} />
+                </Card>
+              </Grid>
+              <Grid item xs={6} md={2.5} className='flex justify-center '>
+                <Card className='rounded-2xl h-[400px] w-[275px]'>
+                  <MediaCard media_data={newsMedia} />
+                </Card>
+              </Grid>
+            </Grid>
           </div>
         </div>
       </main>
