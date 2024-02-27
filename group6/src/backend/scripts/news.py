1e93fb8b27926c2b520616d8e6ff84f2de2234cf
chumaanigbogu <chumaanigbogu@utexas.edu>
2024-02-27 17:48:31 -0600

    Parsing for one instance of news, done

--- group6/src/backend/scripts/news.py
+++ group6/src/backend/scripts/news.py
@@ -14,35 +14,77 @@ def getNews():
     # Setup Selenium WebDriver
 
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  
-    options.add_argument('--disable-gpu')  
-    options.add_argument('--no-sandbox')  
-    options.add_argument('--disable-dev-shm-usage')  
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
 
-    url = 'https://www.rescue.org/search/site/Ukrainian%20refugees?page=0'
+    url = "https://www.rescue.org/announcement/welcoming-our-ukrainian-neighbors-washington"
     driver.get(url)
 
     try:
         # Wait until the element you want to interact with is loaded
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, 'body'))
+            EC.presence_of_element_located((By.TAG_NAME, "body"))
         )
 
         # Now using Selenium to interact with the page
-        scraper = BeautifulSoup(driver.page_source, 'html.parser')
-        stories = scraper.find_all(class_='rplc-teaser-search')
+        scraper = BeautifulSoup(driver.page_source, "html.parser")
 
-        for story in stories:
+        # These lines grab the full title, full story, partial url fo images, date, location, type of source, publish/source
+        images = scraper.find_all(class_="rpla-responsive-image")
+        titles = scraper.find_all(
+            class_="rpla-responsive-title rpla-responsive-title--long"
+        )
+        stories = scraper.find_all(class_="rpla-paragraph rpla-paragraph--body")
+        date = scraper.find_all(class_="rpla-meta")
+        source = scraper.find_all(class_="rplm-caption__credit")
+        type = scraper.find_all(class_="rpla-slug rpla-slug--size-large")
+
+        countries_list = ["Poland", "Ukraine", "Moldova", "Switzerland", "Spain"]
+        unique_countries_set = set()
+
+        # stories = scraper.find_all(class_='rplc-teaser-search')
+
+        for story in titles:
             print(story.text)
-            
 
+        print(" ")
+        print(date[0].text)
+        print(" ")
+        print(source[0].text)
+        print(" ")
 
-        # Example of clicking a link (modify the selector as per your requirement)
-        # link = driver.find_element_by_link_text('Link Text Here')
-        # link.click()
+        # if type[0].text != "Report" or type[0].text != "Annoucement":
+        #     print("Article")
+        # else:
+        print(type[0].text)
 
-        # Add more navigation or interaction code here
+        print(" ")
+        for text in stories:
+            print(text.text)
+
+        # ... (previous code remains unchanged)
+        print(" ")
+        # Loop through story to find location, if no location is found, default loaction to Ukraine
+        for i, text in enumerate(stories):
+            found_countries = [
+                country for country in countries_list if country in text.text
+            ]
+
+            for country in found_countries:
+                unique_countries_set.update(found_countries)
+        if not unique_countries_set:
+            unique_countries_set.add("Ukraine")
+
+        print(unique_countries_set)
+
+    # Example of clicking a link (modify the selector as per your requirement)
+    # link = driver.find_element_by_link_text('Link Text Here')
+    # link.click()
+
+    # Add more navigation or interaction code here
 
     except NoSuchElementException:
         print("Element not found")
@@ -50,4 +92,5 @@ def getNews():
         # Clean up, close the browser
         driver.quit()
 
-    return stories[1].text
+    # gives you the full url for images
+    return "www.rescue.org" + images[0].attrs["src"]
