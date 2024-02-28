d2b5fb227118e07a2cbe6d3269363241b27056c4
chumaanigbogu <chumaanigbogu@utexas.edu>
2024-02-28 16:21:36 -0600

    Update Scraping

--- group6/src/backend/scripts/news.py
+++ group6/src/backend/scripts/news.py
@@ -20,7 +20,7 @@ def getNews():
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
 
-    url = "https://www.rescue.org/announcement/welcoming-our-ukrainian-neighbors-washington"
+    url = "https://www.rescue.org/press-release/irc-71-million-people-are-now-internally-displaced-ukraine"
     driver.get(url)
 
     try:
@@ -32,12 +32,20 @@ def getNews():
         # Now using Selenium to interact with the page
         scraper = BeautifulSoup(driver.page_source, "html.parser")
 
+        images = []
+        titles = []
+        stories = []
+        types = []
+        locations = []
+        sources = []
+        dates = []
+
         # These lines grab the full title, full story, partial url fo images, date, location, type of source, publish/source
-        images = scraper.find_all(class_="rpla-responsive-image")
-        titles = scraper.find_all(
+        image = scraper.find_all(class_="rpla-responsive-image")
+        title = scraper.find_all(
             class_="rpla-responsive-title rpla-responsive-title--long"
         )
-        stories = scraper.find_all(class_="rpla-paragraph rpla-paragraph--body")
+        story = scraper.find_all(class_="rpla-paragraph rpla-paragraph--body")
         date = scraper.find_all(class_="rpla-meta")
         source = scraper.find_all(class_="rplm-caption__credit")
         type = scraper.find_all(class_="rpla-slug rpla-slug--size-large")
@@ -47,27 +55,6 @@ def getNews():
 
         # stories = scraper.find_all(class_='rplc-teaser-search')
 
-        for story in titles:
-            print(story.text)
-
-        print(" ")
-        print(date[0].text)
-        print(" ")
-        print(source[0].text)
-        print(" ")
-
-        # if type[0].text != "Report" or type[0].text != "Annoucement":
-        #     print("Article")
-        # else:
-        print(type[0].text)
-
-        print(" ")
-        for text in stories:
-            print(text.text)
-
-        # ... (previous code remains unchanged)
-        print(" ")
-        # Loop through story to find location, if no location is found, default loaction to Ukraine
         for i, text in enumerate(stories):
             found_countries = [
                 country for country in countries_list if country in text.text
@@ -78,7 +65,56 @@ def getNews():
         if not unique_countries_set:
             unique_countries_set.add("Ukraine")
 
-        print(unique_countries_set)
+        if images:
+            fullUrl = "www.rescue.org" + image[0].attrs["src"]
+            images.append(fullUrl)
+
+        if dates:
+            dates.append(date[0].text.strip())
+
+        if locations:
+            locations.append(unique_countries_set)
+
+        if sources:
+            sources.append(source[0].text)
+
+        for text in story:
+            stories.append(text.text)
+
+        if types:
+            types.append(type[0].text)
+
+        titles.append(title[0].text.strip())
+
+        if not images:
+            images.append("Random Link")
+        print(images)
+        print(" ")
+        print(" ")
+        print(titles)
+        print(" ")
+        print(" ")
+        if not types:
+            types.append("Article")
+        print(types)
+        print(" ")
+        print(" ")
+        if not dates:
+            dates.append("Feburary 22nd,2022")
+        print(dates)
+        print(" ")
+        print(" ")
+        if not sources:
+            sources.append("Resource.org")
+        print(sources)
+        print(" ")
+        print(" ")
+        if not locations:
+            locations.append("Ukraine")
+        print(locations)
+        print(" ")
+        print(" ")
+        print(stories)
 
     # Example of clicking a link (modify the selector as per your requirement)
     # link = driver.find_element_by_link_text('Link Text Here')
@@ -93,4 +129,4 @@ def getNews():
         driver.quit()
 
     # gives you the full url for images
-    return "www.rescue.org" + images[0].attrs["src"]
+    return "HI"
