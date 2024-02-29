abacf3996a544e580e5abaf0d73f390c6d001c85
Derek Chen <derex1987@gmail.com>
2024-02-29 02:49:26 -0600

    More tests to run on postman: git push

--- postman_tests.js
+++ postman_tests.js
@@ -2,3 +2,29 @@ pm.test("Welcome message is returned", function () {
     pm.response.to.have.status(200);
     pm.expect(typeof pm.response.text()).to.equal("string");
 });
+
+pm.test("Check type of response for news articles", function () {
+    pm.response.to.have.status(200);
+    pm.expect(typeof pm.response.json()).to.equal("object");
+});
+
+pm.test("Check type of response for support groups", function () {
+    pm.response.to.have.status(200);
+    pm.expect(typeof pm.response.json()).to.equal("object");
+    let groups = pm.response.json();
+    for (var i = 0; i < groups.length; i++) {
+        let group = groups[i];
+        
+        // Check if each object has 'name', 'link', and 'about' attributes
+        pm.test("Object " + (i+1) + " has 'name', 'link', and 'about' attributes", function () {
+            pm.expect(group).to.have.property('name');
+            pm.expect(group).to.have.property('link');
+            pm.expect(group).to.have.property('about');
+        });
+    }
+});
+
+pm.test("Check type of response for refugee testimonials", function () {
+    pm.response.to.have.status(200);
+    pm.expect(typeof pm.response.json()).to.equal("object");
+});
