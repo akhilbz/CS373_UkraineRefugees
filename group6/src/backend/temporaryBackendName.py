d80a46f8f89e6c747e63207227bb01a9108a8808
Derek Chen <derex1987@gmail.com>
2024-03-03 03:51:45 -0600

    Sample Rest API

--- group6/src/backend/temporaryBackendName.py
+++ group6/src/backend/temporaryBackendName.py
@@ -1,6 +1,14 @@
 # Going to use flask to get from the database and make it a rest API
-from flask import Flask
+from flask import Flask, jsonify, request
 
 flaskApp = Flask(__name__)
 
-@flaskApp.route("/")
+# Sample data 
+temp = [
+    {"id": 1, "title": "News 1", "author": "Author 1"},
+    {"id": 2, "title": "News 2", "author": "Author 2"}
+]
+
+@flaskApp.route("/api/news", methods=['GET'])
+def get_sample():
+    return jsonify(temp)
