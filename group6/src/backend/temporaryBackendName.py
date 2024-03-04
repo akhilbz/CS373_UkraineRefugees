89157d9b2c55857f1000509b02ba5e8996c9d539
Derek Chen <derex1987@gmail.com>
2024-03-04 16:33:37 -0600

    Working on news model

--- group6/src/backend/temporaryBackendName.py
+++ group6/src/backend/temporaryBackendName.py
@@ -1,7 +1,30 @@
 # Going to use flask to get from the database and make it a rest API
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 
 flaskApp = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+flaskApp.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(flaskApp)
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    publishedAt = db.Column(db.DateTime)
+    name = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    urlToImage = db.Column(db.String(255))
+
+
 
 # Sample data 
 temp = [
@@ -11,4 +34,21 @@ temp = [
 
 @flaskApp.route("/api/news", methods=['GET'])
 def get_sample():
-    return jsonify(temp)
+    return jsonify(temp), 200
+
+@flaskApp.route('/api/books', methods=['POST'])
+def create_sample():
+    try:
+        data = request.json
+        new_sample = {
+            'id': len(temp) + 1,
+            'title': data['title'],
+            'author': data['author']
+        }
+        temp.append(new_sample)
+        return jsonify(new_sample), 201
+    except:
+        return 400
+
+if __name__ == "__main__":
+    flaskApp.run(port=5000, debug=True);
