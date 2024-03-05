0a118c037c18f106fd2453fa783d4bfd9060db3d
chumaanigbogu <chumaanigbogu@utexas.edu>
2024-03-04 19:06:19 -0600

    Update flask for db model for country

--- group6/src/backend/application.py
+++ group6/src/backend/application.py
@@ -26,8 +26,18 @@ class NewsModel(db.Model):
     published_at = db.Column(db.DateTime, nullable=False)
     source_name = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    image_url = db.Column(db.String(255), nullable=True)
+    image_url = db.Column(db.Text, nullable=True)
     date_added = db.Column(db.DateTime, nullable=False)
+
+
+class CountryModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    capital = db.Column(db.String(255), nullable=False)
+    region = db.Column(db.String(255), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    languages = db.Column(db.String(255), nullable=False)
+    flag = db.Column(db.Text, nullable=True)
 
 
 @application.route("/")
