7bb221981e5227fff710292777f83b446b1b15ca
chumaanigbogu <chumaanigbogu@utexas.edu>
2024-03-07 00:11:50 -0600

    Front End Tests Done

--- group6/src/pages/FrontEndTests.py
+++ group6/src/pages/FrontEndTests.py
@@ -48,21 +48,101 @@ class Test(unittest.TestCase):
         self.driver.get(URL + extension)
         self.assertEqual(self.driver.current_url, URL + extension)
 
-    # def test5(self):
-    #     WebDriverWait(self.driver, 30).until(
-    #         EC.presence_of_element_located((By.NAME, 'About')))
-    #     link = self.driver.find_element(By.NAME, "About")
+    def test5(self):
+        WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[text()='About']")))
+        link = self.driver.find_element(By.XPATH, "//div[text()='About']")
+        link.click()
+        WebDriverWait(self.driver, 10).until(EC.url_matches(URL + "about-us"))
+        self.assertEqual(self.driver.current_url, URL + "about-us")
 
-    # # If the link is inside an iframe, switch to it before interacting with the element
-    # # self.driver.switch_to.frame("iframe_name_or_id")
+    def test6(self):
+        WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[text()='News and Media']")))
+        link = self.driver.find_element(
+            By.XPATH, "//div[text()='News and Media']")
+        link.click()
+        WebDriverWait(self.driver, 30).until(
+            EC.url_matches(URL + "news"))
+        self.assertEqual(self.driver.current_url, URL + "news")
 
-    #     link.click()
+    def test7(self):
+        WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[text()='Support Groups']")))
+        link = self.driver.find_element(
+            By.XPATH, "//div[text()='Support Groups']")
+        link.click()
+        WebDriverWait(self.driver, 30).until(
+            EC.url_matches(URL + "support-groups"))
+        self.assertEqual(self.driver.current_url, URL + "support-groups")
 
-    # # If you switched to an iframe, switch back to the default content
-    # # self.driver.switch_to.default_content()
+    def test8(self):
 
-    #     WebDriverWait(self.driver, 10).until(EC.url_matches(URL + "about-us"))
-    #     self.assertEqual(self.driver.current_url, URL + "about-us")
+        original_window_handle = self.driver.current_window_handle
+
+        WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[text()='About']")))
+        link = self.driver.find_element(By.XPATH, "//div[text()='About']")
+
+        # Click the link which opens a new tab
+        link.click()
+
+        # Switch to the new tab
+        new_window_handle = None
+        for window_handle in self.driver.window_handles:
+            if window_handle != original_window_handle:
+                new_window_handle = window_handle
+                break
+
+        if new_window_handle:
+            self.driver.switch_to.window(new_window_handle)
+
+            # Find the link in the new tab using the new window handle
+            link_in_new_tab = WebDriverWait(self.driver, 30).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "AboutUs_documentationLinkBox__aTYWK")))
+            link_in_new_tab.click()
+
+            # Wait for the new URL in the new tab
+            WebDriverWait(self.driver, 10).until(EC.url_matches(
+                "https://gitlab.com/ajimenez1173/cs373-group-6"))
+
+            # Assert the new URL
+            self.assertEqual(self.driver.current_url,
+                             "https://gitlab.com/ajimenez1173/cs373-group-6")
+
+    def test9(self):
+
+        original_window_handle = self.driver.current_window_handle
+
+        WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//div[text()='About']")))
+        link = self.driver.find_element(By.XPATH, "//div[text()='About']")
+
+        # Click the link which opens a new tab
+        link.click()
+
+        # Switch to the new tab
+        new_window_handle = None
+        for window_handle in self.driver.window_handles:
+            if window_handle != original_window_handle:
+                new_window_handle = window_handle
+                break
+
+        if new_window_handle:
+            self.driver.switch_to.window(new_window_handle)
+
+            # Find the link in the new tab using the new window handle
+            link_in_new_tab = WebDriverWait(self.driver, 30).until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "AboutUs_documentationLink__dgihN")))
+            link_in_new_tab.click()
+
+            # Wait for the new URL in the new tab
+            WebDriverWait(self.driver, 10).until(EC.url_matches(
+                "https://documenter.getpostman.com/view/32956503/2sA2r53kYq"))
+
+            # Assert the new URL
+            self.assertEqual(self.driver.current_url,
+                             "https://documenter.getpostman.com/view/32956503/2sA2r53kYq")
 
 
 if __name__ == "__main__":
