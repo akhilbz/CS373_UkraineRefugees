b072183ba520effc31ae8e2c4e08dabf2bf9428b
AlexJimenez12 <ajimenez1173@gmail.com>
2024-04-24 23:03:11 -0500

    Adding Video to the website

--- group6/src/pages/about-us.js
+++ group6/src/pages/about-us.js
@@ -365,6 +365,22 @@ export default function AboutUs() {
                             </div>
                         ))}
                     </div>
+
+                    <div className={styles.pechaKuchaContainer}>
+                        <h1 className={styles.pechaKuchaHeader}>Our PechaKucha</h1>
+                        <div className={styles.youtubeVideo}>
+                            <iframe
+                                width="560"
+                                height="315"
+                                src="https://www.youtube.com/embed/6NxcCM8JuJ0?si=ULd_KTVWjtwKOj9F"
+                                title="YouTube video player"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen>
+                            </iframe>
+                        </div>
+                    </div>
                     
                 </div>
             </main>
